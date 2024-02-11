f40242b7620c458dff0573b88e3ecf986d6acd8c
Ashish Kumar <127542246+ashishyou@users.noreply.github.com>
2024-02-11 12:16:55 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("****************************"); 
+		System.out.println("------------Welcome---------"); 
+		System.out.println("****************************");  
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
